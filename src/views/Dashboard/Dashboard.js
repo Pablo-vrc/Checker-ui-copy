ea6fec091a534c5733e4d7a77211883408078eb1
OneShootMan <pablovrc@umich.edu>
2024-09-29 05:12:51 -0400

    I just woke up

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -33,7 +33,7 @@ import {
 	Spacer,
 	Stack,
 	VStack,
-	Hstack,
+	HStack,
 	Stat,
 	StatHelpText,
 	StatLabel,
@@ -46,44 +46,19 @@ import {
 	InputGroup,
 	Tr
 } from '@chakra-ui/react';
-import {ArrowUpIcon} from '@chakra-ui/icons'
+import { ArrowUpIcon } from '@chakra-ui/icons';
 // Custom components
 import Card from 'components/Card/Card.js';
 import CardBody from 'components/Card/CardBody.js';
 import CardHeader from 'components/Card/CardHeader.js';
-import BarChart from 'components/Charts/BarChart';
-import LineChart from 'components/Charts/LineChart';
-import IconBox from 'components/Icons/IconBox';
-// Icons
-import { CartIcon, DocumentIcon, GlobeIcon, RocketIcon, StatsIcon, WalletIcon } from 'components/Icons/Icons.js';
-import DashboardTableRow from 'components/Tables/DashboardTableRow';
-import TimelineRow from 'components/Tables/TimelineRow';
 import React from 'react';
 import { AiFillCheckCircle } from 'react-icons/ai';
-import { BiHappy } from 'react-icons/bi';
-import { BsArrowRight } from 'react-icons/bs';
-import { IoCheckmarkDoneCircleSharp, IoEllipsisHorizontal } from 'react-icons/io5';
 // Data
-import {
-	barChartDataDashboard,
-	barChartOptionsDashboard,
-	lineChartDataDashboard,
-	lineChartOptionsDashboard
-} from 'variables/charts';
 import { dashboardTableData, timelineData } from 'variables/general';
 
 export default function Dashboard() {
 	return (
-		<Flex flexDirection='column' pt={{ base: '120px', md: '75px' }}>
-			<SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing='4px'>
-				{/* MiniStatistics Card */}
-				
-				{/* MiniStatistics Card */}
-				
-				{/* MiniStatistics Card */}
-				
-				{/* MiniStatistics Card */}
-							</SimpleGrid>
+		<Flex flexDirection='column'>
 			<Grid templateColumns={{ sm: '1fr', md: '1fr 1fr', '2xl': '2fr 1.2fr 1.5fr' }} my='26px' gap='18px'>
 				{/* Welcome Card */}
 				<Card
@@ -95,75 +70,42 @@ export default function Dashboard() {
 						<Flex flexDirection={{ sm: 'column', lg: 'row' }} w='100%' h='100%'>
 							<Flex flexDirection='column' h='100%' p='22px' minW='60%' lineHeight='1.6'>
 								<Text fontSize='28px' color='#fff' fontWeight='bold' mb='6px'>
-									Name:  Mark Johnson
+									Name: Mark Johnson
 								</Text>
 								<Text fontSize='24px' color='white' fontWeight='normal'>
-									Age:  22 years old
+									Age: 22 years old
 								</Text>
 								<Text fontSize='md' color='white' fontWeight='normal' mb='auto'>
-									Native Lenguage:  Spanish
+									Native Language: Spanish
 								</Text>
 								<Spacer />
 							</Flex>
-							 <Flex
-                flexDirection='column'
-                h='100%'
-                p='22px'
-                minW='40%' // Adjust width as needed
-                justifyContent='space-between' // This will space the items evenly
-            >
-				<Box mb='10px'>
-					<Text fontSize='20px' color='white' fontWeight='bold'> Address:  1808 Duffield rd </Text>
-				</Box>
-
-				<Box mb='10px'>
-					<Text fontSize='20px' color='white' fontWeight='bold'> Weight:  140 lbs </Text>
-				</Box>
-
-				<Box mb='10px'>
-					<Text fontSize='20px' color='white' fontWeight='bold'> Height:  5'9 </Text>
-				</Box>
-
-			</Flex>
+							<Flex
+								flexDirection='column'
+								h='100%'
+								p='22px'
+								minW='40%' // Adjust width as needed
+								justifyContent='space-between' // This will space the items evenly
+							>
+								<Box mb='10px'>
+									<Text fontSize='20px' color='white' fontWeight='bold'> Address: 1808 Duffield rd </Text>
+								</Box>
+								<Box mb='10px'>
+									<Text fontSize='20px' color='white' fontWeight='bold'> Weight: 140 lbs </Text>
+								</Box>
+								<Box mb='10px'>
+									<Text fontSize='20px' color='white' fontWeight='bold'> Height: 5'9 </Text>
+								</Box>
+							</Flex>
 						</Flex>
 					</CardBody>
 				</Card>
-				{/* Satisfaction Rate */}
-				<Card gridArea={{ md: '2 / 1 / 3 / 2', '2xl': 'auto' }} maxW='70%'>
-					<CardHeader mb='24px'>
-						<Flex flexDirection={{ sm: 'column', lg: 'row' }} w='100%' h='100%' alignItems='center'>
-							<Text fontSize='15px' color='white'> 12:26pm </Text>	
-							<Spacer />
-							<Icon as={AiFillCheckCircle} color='green.500' w='15px' h='15px' me='5px' />			
-							<Text fontSize='15px' color='white' > Online </Text>	
-						</Flex>
-					</CardHeader>
-					<Flex direction='column' justify='center' align='center' h='100%'>
-						<Box zIndex='10' boxSize='80%'>
-							<Image src='https://pm1.aminoapps.com/7544/d523bfe83733e68a0f353744a72131efd890b496r1-500-670v2_hq.jpg' alt='rat' objectFit="cover" boxSize="100%">
-							</Image>	
-						</Box>
-						
-					</Flex>
-				</Card>
-				{/* Referral Tracking */}
-				<Card gridArea={{ md: '2 / 2 / 3 / 3', '2xl': 'auto' }}>
-					<Flex direction='column'>
-						<Flex direction={{ sm: 'column', md: 'row' }}>
-							<Box boxSize='100p%'>
-								<Image src='https://media.wired.com/photos/5a6a61938c669c70314b300d/master/pass/Google-Map-US_10.jpg' alt='location' objectFit="cover" boxSize="100%">
-								</Image>
-							</Box>	
-						</Flex>
-					</Flex>
-				</Card>
 			</Grid>
-			
-			<Grid templateColumns={{ sm: '1fr', md: '1fr 1fr', lg: '2fr 1fr' }} gap='24px'>
-				{/* Projects */}
-				
-				{/* Orders Overview */}
-				<Card maxW='50%'>
+
+			{/* Chat, Live Stream, and Geolocation Cards Side by Side */}
+			<Grid templateColumns={{ sm: '1fr', md: '1fr 1fr 1fr' }} gap='24px' my='26px'>
+				{/* Chat Card */}
+				<Card>
 					<CardHeader mb='32px'>
 						<Flex direction='column'>
 							<Text fontSize='lg' color='#fff' fontWeight='bold' mb='6px'>
@@ -177,33 +119,66 @@ export default function Dashboard() {
 							</Flex>
 						</Flex>
 					</CardHeader>
-					<CardBody>
-					{/*Online chat here*/}	
+					<CardBody display='flex' flexDirection='column' justifyContent='flex-end' height='100%'>
 						<VStack align='start' spacing={4}>
 							<Box bg='gray.700' p={3} borderRadius='lg' maxW='75%'>
-							<Text color='white'>User: Hi! How can I help you today?</Text>
+								<Text color='white'>User: Hi! How can I help you today?</Text>
 							</Box>
 							<Box bg='blue.600' p={3} borderRadius='lg' maxW='75%' alignSelf='end'>
-							<Text color='white'>You: I'm just testing the chat feature.</Text>
+								<Text color='white'>You: I'm just testing the chat feature.</Text>
 							</Box>
 							<Box bg='gray.700' p={3} borderRadius='lg' maxW='75%'>
-							<Text color='white'>User: Looks good! Let me know if you need anything.</Text>
+								<Text color='white'>User: Looks good! Let me know if you need anything.</Text>
 							</Box>
 							<InputGroup>
 								<Input placeholder='Type a message...' size='md' color='white' bg='gray.800' />
 								<Button
 									bg='blue.600'
 									border='none'
-									_hover={{bg: 'blue.700'}}
-									_active={{bg: 'transparent'}}	
-									>
+									_hover={{ bg: 'blue.700' }}
+									_active={{ bg: 'transparent' }}>
 									<ArrowUpIcon color='white' />
 								</Button>
 							</InputGroup>
-						</VStack>	
+						</VStack>
+					</CardBody>
+				</Card>
+
+				{/* Live Stream Card */}
+				<Card>
+					<CardHeader mb='32px'>
+						<Flex flexDirection={{ sm: 'column', lg: 'row' }} w='100%' h='100%' alignItems='center'>
+							<Text fontSize='15px' color='white'> 12:26pm </Text>
+							<Spacer />
+							<Icon as={AiFillCheckCircle} color='green.500' w='15px' h='15px' me='5px' />
+							<Text fontSize='15px' color='white'> Online </Text>
+						</Flex>
+					</CardHeader>
+					<CardBody>
+						{/* Add live stream content here */}
+						<Image src='https://pm1.aminoapps.com/7544/d523bfe83733e68a0f353744a72131efd890b496r1-500-670v2_hq.jpg' />
+					</CardBody>
+				</Card>
+
+				{/* Geolocation Card */}
+				<Card>
+					<CardHeader mb='32px'>
+						<Flex direction='column' w='100%' h='100%'>
+							<Text fontSize='lg' color='#fff' fontWeight='bold' mb='6px'>
+								Geolocation
+							</Text>
+						</Flex>
+					</CardHeader>
+					<CardBody p={0} display="flex" justifyContent="center" alignItems="center">
+						{/* Add geolocation content here */}
+						<Image src='https://media.wired.com/photos/5a6a61938c669c70314b300d/master/pass/Google-Map-US_10.jpg'
+						boxSize="100%" 
+						objectFit="cover" 
+						borderRadius="md" />
 					</CardBody>
 				</Card>
 			</Grid>
 		</Flex>
 	);
 }
+
